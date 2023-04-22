90e3df4a075622f240ad609d5adb77b58826c5b2
Rodrigo270103 <91211085+Rodrigo270103@users.noreply.github.com>
2023-04-22 03:53:47 +0000

    If para posicionar el elemnto y saber si es padre o hijo dentro de la eestructura del arbol

--- student_file.c
+++ student_file.c
@@ -20,6 +20,48 @@ tempNode->value = data;
    tempNode->left = NULL;
    tempNode->right = NULL;
 
+if(root == NULL) {
+        //Codigo
+        root = tempNode;
+        actual = tempNode;
+        padre = tempNode;
+   } else {
+        //Codigo
+        actual = root;
+        padre = NULL;
+
+
+      while(1) {
+         padre = actual;
+         //recorrido por la izquierda del árbol
+
+
+         if(data < padre->value) {
+           //Codigo
+           actual = actual->left;
+
+
+            //insertamos en izquierda
+            if(actual == NULL) {
+               //Codigo
+               padre->left = tempNode;
+               return;
+            }
+         }
+         //recorrido por la derecha del árbol
+         else {
+                //Codigo
+                actual = actual->right;
+            //insertamos en derecha
+            if(actual == NULL) {
+                //Codigo
+                padre->right = tempNode;
+                return;
+            }
+         }
+      }
+   }
+}
 
   
 
